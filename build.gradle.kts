be4afe9eaca8a33cad11eba39a77f4762bd944d7
hope <two_somang@icloud.com>
2022-09-20 02:27:03 +0900

    프론트 작업 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("io.github.openfeign:feign-core:11.9.1")
+    implementation("io.github.openfeign:feign-gson:11.9.1")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
     compileOnly("org.projectlombok:lombok")
     implementation("mysql:mysql-connector-java")
---
+++ src/main/java/com/ming/payments/model/PaymentApprovalRequest.java
@@ -0,0 +1,29 @@
+package com.ming.payments.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PaymentApprovalRequest {
+
+    /**
+     * 결제 건에 대한 고유한 키값입니다. 최대 길이는 200자입니다.
+     */
+    private String paymentKey;
+
+    /**
+     * 상점에서 주문 건을 구분하기 위해 발급한 고유 ID입니다. 영문 대소문자, 숫자, 특수문자(-, _)로 이루어진 6자 이상 64자 이하의 문자열이어야 합니다.
+     */
+    private String orderId;
+
+    /**
+     * 결제할 금액입니다.
+     */
+    private Long amount;
+
+}
