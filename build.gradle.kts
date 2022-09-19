d741f45fe229b17bb5a07b994e00364150822780
hope <two_somang@icloud.com>
2022-09-20 00:22:29 +0900

    프로젝트 환경 구성

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("org.springframework.boot") version ("2.7.3")
+    id("io.spring.dependency-management") version ("1.0.13.RELEASE")
+    id("java")
+}
+
+group = "com.ming"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+    targetCompatibility = JavaVersion.VERSION_11
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    compileOnly("org.projectlombok:lombok")
+    implementation("mysql:mysql-connector-java")
+    annotationProcessor("org.projectlombok:lombok")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
