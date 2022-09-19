a3ea36431ff0079bfc73601f916a72a13aeac42e
hope <hope@gravylab.co.kr>
2022-09-19 21:26:01 +0900

    initial commit

---
+++ src/main/java/com/ming/payments/TossPaymentsPocApplication.java
@@ -0,0 +1,13 @@
+package com.ming.payments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TossPaymentsPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TossPaymentsPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ming/payments/TossPaymentsPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ming.payments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TossPaymentsPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
