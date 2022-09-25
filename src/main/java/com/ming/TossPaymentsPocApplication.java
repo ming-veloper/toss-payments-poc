fc26143f9d2902b888db98b85bad172c7bf02e87
hope <hope@gravylab.co.kr>
2022-09-25 16:03:26 +0900

    테스트를 위해 모든 시스템 환경 변수 출력하는 문 작성

--- src/main/java/com/ming/TossPaymentsPocApplication.java
+++ src/main/java/com/ming/TossPaymentsPocApplication.java
@@ -12,13 +12,20 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.StandardEnvironment;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import java.util.Map;
 
 @EnableAsync
 @SpringBootApplication
 public class TossPaymentsPocApplication {
 
     public static void main(String[] args) {
+        Map<String, Object> systemEnvironment = new StandardEnvironment().getSystemEnvironment();
+        for (String s : systemEnvironment.keySet()) {
+            System.out.println(s + " : " + systemEnvironment.get(s));
+        }
         SpringApplication.run(TossPaymentsPocApplication.class, args);
     }
 
